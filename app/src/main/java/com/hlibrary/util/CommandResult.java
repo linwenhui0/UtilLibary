c255e97d3badbf5c053346ebe18ffde57f586fca
linwenhui <1252908234@qq.com>
2018-03-06 16:53:35 +0800

    +）增加执行Shell方法

---
+++ app/src/main/java/com/hlibrary/util/CommandResult.java
@@ -0,0 +1,30 @@
+package com.hlibrary.util;
+
+/**
+ * Created by linwenhui on 2018/3/5.
+ */
+
+public class CommandResult {
+    /** result of command **/
+    public int exitCode;
+    /** success message of command result **/
+    public String successMsg;
+    /** error message of command result **/
+    public String errorMsg;
+
+    public CommandResult(int result) {
+        this.exitCode = result;
+    }
+
+    public CommandResult(int result, String successMsg, String errorMsg) {
+        this.exitCode = result;
+        this.successMsg = successMsg;
+        this.errorMsg = errorMsg;
+    }
+
+    @Override
+    public String toString() {
+        return "exitCode=" + exitCode + "; successMsg=" + successMsg
+                + "; errorMsg=" + errorMsg;
+    }
+}
